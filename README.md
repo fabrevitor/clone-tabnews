164a07026690b309867226271209c05a7c62d5d2
Vitor Fabre de Souza <vitor_fabre@hotmail.com>
2026-01-09 23:26:44 +0000

    add lint scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Clone Tab News project - curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>FIGUEIRENSE!!</h1>
+  return <h1>FIGUEIRENSE!!</h1>;
 }
 
 export default Home;
